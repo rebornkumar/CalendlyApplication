3ebafcd8511af5f95e31c1a6c7f50a53c03d6ba5
Anuj Kumar <iitr.anujkumar@gmail.com>
2020-04-19 03:49:36 +0530

    First commit

---
+++ src/main/java/com/learn/CalendlyApplication/CalendlyApplication.java
@@ -0,0 +1,13 @@
+package com.learn.CalendlyApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/CalendlyApplication/CalendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.CalendlyApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
